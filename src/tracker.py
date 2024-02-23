266d4ab3f8a97a3740442fd6cb236a3a38569526
longthp <phamlong.th02@gmail.com>
2024-02-23 07:38:53 +0700

    fix: update custom logger and sqlalchemy engine
    - use custom logger object (`tracker`)
    - remove sqlalchemy engine parameter from `TrackerRunner`

--- src/tracker.py
+++ src/tracker.py
@@ -6,8 +6,6 @@ import random
 import httpx
 import tenacity
 
-from sqlalchemy import Engine
-
 from src import headers
 from src import logger
 from src import parser
@@ -16,17 +14,18 @@ from src.db import utils
 from src.config import settings
 
 logger.init_logger()
+custom_logger = logger.get_logger("tracker")
 
 HEADERS_HANDLER = headers.HeadersHandler()
 
 
 def custom_retry_callback(retry_state: tenacity.RetryCallState) -> None:
     if retry_state.attempt_number < settings.MAX_RETRIES:
-        logger.get_logger("tenacity").info(
+        custom_logger.info(
             msg=f"retrying: {retry_state.attempt_number} time(s)"
         )
     else:
-        logger.get_logger("tenacity").warning(
+        custom_logger.warning(
             msg=f"reached retries limit: {retry_state.attempt_number} time(s)"
         )
 
@@ -74,15 +73,19 @@ async def async_request(
         response = response.raise_for_status()
 
         if response is None:
-            logger.get_logger("general").warning(
-                msg=f"emptry response {url}"
+            custom_logger.warning(
+                msg=f"http request: emptry response {url}"
             )
-        
-        return response
+        else:
+            custom_logger.info(
+                msg=f"http request ({response.status_code}): {method} {url} {kwargs}"
+            )
+            return response
 
     except httpx.HTTPStatusError as e:
-        logger.get_logger("httpx").warning(
-            msg=f"failed with status: {e.response.status_code} {url}"
+        custom_logger.warning(
+            msg=f"http request ({e.response.status_code}): {method} {url}",
+            extra={"url": url, "status_code": e.response.status_code}
         )
         if e.response.status_code == 404:
             pass
@@ -94,16 +97,16 @@ async def async_request(
         else:
             raise tenacity.TryAgain
 
-    except httpx.ReadTimeout:
-        logger.get_logger("httpx").error(msg=f"failed with timeout: {url}")
+    except httpx.ReadTimeout as e:
+        custom_logger.error(msg=f"failed with timeout: {url}")
         raise tenacity.TryAgain
 
     except httpx.RemoteProtocolError:
-        logger.get_logger("httpx").error(msg=f"server not response: {url}")
+        custom_logger.error(msg=f"server not response: {url}")
         raise tenacity.TryAgain
 
     except httpx.UnsupportedProtocol:
-        logger.get_logger("httpx").error(msg=f"unsupported protocol: {url}")
+        custom_logger.error(msg=f"unsupported protocol: {url}")
         raise tenacity.TryAgain
 
 
@@ -117,12 +120,9 @@ class TrackerRunner:
         self.client = client
         self.sem = sem
         self.config = tracker_config
+        self.engine = utils.get_engine(self.config.sqlite_uri)
 
-    async def proces_one(
-        self,
-        product: dict,
-        engine: Engine
-    ) -> None:
+    async def proces_one(self, product: dict) -> None:
         async with self.sem:
             url = ""
 
@@ -141,20 +141,21 @@ class TrackerRunner:
                 url=url,
                 headers=custom_headers
             )
-            assert response is not None
 
             values = []
 
-            custom_parser = self.config.parser_class(
-                response=response
-            )
-            values = custom_parser.parse()
+            if response is not None:
 
-            utils.upsert_many(
-                engine=engine,
-                values=values,
-                instance=models.ShopifyInventory
-            )
+                custom_parser = self.config.parser_class(
+                    response=response
+                )
+                values = custom_parser.parse()
+
+                utils.upsert_many(
+                    engine=self.engine,
+                    values=values,
+                    instance=models.ShopifyInventory
+                )
 
             if self.sem.locked():
                 sleep_delay = async_sleep_random(
@@ -164,9 +165,7 @@ class TrackerRunner:
 
         return
 
-    async def process_many(
-        self, engine: Engine
-    ) -> None:
+    async def process_many(self) -> None:
         page_number = 1
 
         while True:
@@ -200,13 +199,13 @@ class TrackerRunner:
             pparser.parse_products()
 
             utils.upsert_many(
-                engine=engine,
+                engine=self.engine,
                 values=pparser.products,
                 instance=models.ShopifyProduct
             )
 
             utils.upsert_many(
-                engine=engine,
+                engine=self.engine,
                 values=pparser.variants,
                 instance=models.ShopifyVariant
             )
@@ -216,17 +215,16 @@ class TrackerRunner:
     async def run(
         self, skip_products: bool = False
     ) -> None:
-        engine = utils.get_engine(url=self.config.sqlite_uri)
 
         utils.init_database(
-            engine=engine, metadata=models.ShopifyBase.metadata
+            engine=self.engine, metadata=models.ShopifyBase.metadata
         )
 
         if not skip_products:
-            await self.process_many(engine)
+            await self.process_many()
 
         product_items = utils.select_by_column_names(
-            engine=engine,
+            engine=self.engine,
             table_name="product",
             colnames=["id", "url"]
         )
@@ -234,7 +232,7 @@ class TrackerRunner:
         try:
             tasks = [
                 asyncio.create_task(
-                    self.proces_one(product=p, engine=engine)
+                    self.proces_one(product=p)
                 )
                 for p in product_items
             ]
