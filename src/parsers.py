86bce159b0c4a76a8e47574eefe0544ac4fb3095
longthp <phamlong.th02@gmail.com>
2024-03-04 16:16:49 +0700

    feat: add new custom parser classes
    implement `typing.Protocol` for base `HTMLParser` interface

--- src/parsers.py
+++ src/parsers.py
@@ -12,6 +12,9 @@ import bs4
 import httpx
 
 from typing import NamedTuple
+from typing import Protocol
+from typing import runtime_checkable
+from typing import Any
 
 from src.config import settings
 
@@ -200,27 +203,25 @@ def filter_dict_items(
 class ShopifyProductsParser:
     def __init__(
         self,
-        response: dict[str, dict],
-        tracker_config: TrackerConfig
+        response: dict[str, Any],
     ):
         self.response = response
-        self.config = tracker_config
-        self.products = []
-        self.variants = []
+        self.products: list[dict] = []
+        self.variants: list[dict] = []
 
-    def parse_products(self) -> None:
+    def parse_products(self, base_url: str) -> None:
         product_items = self.response["products"]
         assert len(product_items) > 0
 
         for p in product_items:
             product = filter_dict_items(
-                item=p, includes=REQUIRED_PRODUCT_COLUMNS
+                item=p, includes=_REQUIRED_PRODUCT_COLUMNS
             )
 
             handle = product.get("handle")
             if handle is not None:
                 product_config = Product(
-                    base_url=self.config.base_url,
+                    base_url=base_url,
                     handle=handle
                 )
                 product.update({"url": product_config.url})
@@ -236,26 +237,19 @@ class ShopifyProductsParser:
     def parse_variants(self, variants: list[dict]) -> None:
         for v in variants:
             variant = filter_dict_items(
-                item=v, includes=REQUIRED_VARIANT_COLUMNS
+                item=v, includes=_REQUIRED_VARIANT_COLUMNS
             )
             self.variants.append(variant)
 
 
 class JSONParser:
-    def __init__(
-        self,
-        response: httpx.Response
-    ) -> None:
-        self.response = response
-
-    def parse(self) -> list[dict]:
-        data = self.response.json()
-        variant_details = data["product"]["variants"]
+    def parse(self, markup: str | bytes) -> list[dict]:
+        variant_details = json.loads(markup)["product"]["variants"]
         inventory = []
 
         for v in variant_details:
             amount = filter_dict_items(
-                item=v, includes=REQUIRED_INVENTORY_COLUMNS
+                item=v, includes=_REQUIRED_INVENTORY_COLUMNS
             )
             amount["variant_id"] = amount["id"]
             amount.pop("id")
@@ -266,33 +260,15 @@ class JSONParser:
 
 
 # custom parser
-class HTMLParser:
-    def __init__(
-        self,
-        response: httpx.Response
-    ) -> None:
-        self.soup = bs4.BeautifulSoup(
-            markup=response.text,
-            features="html.parser"
-        )
-
-    def parse(self) -> list[dict]:
+@runtime_checkable
+class HTMLParser(Protocol):
+    def parse(self, markup: str | bytes) -> list[dict]:
         ...
 
 
 class HTMLSwymParser(HTMLParser):
-    def __init__(
-        self,
-        *args,
-        **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-
-    def get_script(self, selector: str) -> bs4.PageElement:
-            return self.soup.select(selector=selector)[0]
-
-    def filter_script(self) -> list[str]:
-        s = self.get_script("script#swym-snippet")
+    def filter(self, soup: bs4.BeautifulSoup) -> list[str]:
+        s = soup.select(selector="script#swym-snippet")[0]
         raw_variants = []
 
         for i in str(s).split(";"):
@@ -301,9 +277,13 @@ class HTMLSwymParser(HTMLParser):
 
         return raw_variants
 
-    def parse(self) -> list[dict]:
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
         inventory = []
-        for v in self.filter_script():
+
+        for v in self.filter(soup):
             p = v.replace("\n", "").strip().split(",")
 
             variant_id = 0
@@ -311,18 +291,18 @@ class HTMLSwymParser(HTMLParser):
 
             for i in p:
                 if "stk" in i:
-                    inventory_quantity = str(i).strip().rstrip(",").lstrip("stk: ")
+                    q = i.strip().rstrip(",").lstrip("stk: ")
+                    inventory_quantity = int(q)
 
                 if "SwymProductVariants" in i:
-                    variant_id = str(i)
                     t = re.search(
                         pattern=r"(?<=\[).+?(?=\])",
-                        string=variant_id
+                        string=str(i)
                     )
                     
                     assert t is not None
                     s, e = t.span()
-                    variant_id = variant_id[s:e]
+                    variant_id = int(i[s:e])
 
             inventory.append(
                 {
@@ -335,19 +315,9 @@ class HTMLSwymParser(HTMLParser):
 
 
 class HTMLGloboParser(HTMLParser):
-    def __init__(
-        self,
-        *args,
-        **kwargs
-    ) -> None:
-        super().__init__(*args, **kwargs)
-
-    def get_script(self, selector: str) -> bs4.PageElement:
-        return self.soup.select(selector)[0]
-
-    def filter_script(self) -> list[str]:
+    def filter(self, soup: bs4.BeautifulSoup) -> list[str]:
         selector = "script[type='text/javascript'][hs-ignore]"
-        script = self.get_script(selector)
+        script = soup.select(selector)[0]
         raw_variants = []
 
         for i in str(script.text).split(";"):
@@ -359,9 +329,12 @@ class HTMLGloboParser(HTMLParser):
 
         return raw_variants
 
-    def parse(self) -> list[dict]:
+    def parse(self, markup: str | bytes) -> list[dict]:
         inventory = []
-        vs = self.filter_script()
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+        vs = self.filter(soup)
         ps = list(zip(vs[:-1], vs[1:]))[::2]
 
         for v in ps:
@@ -379,25 +352,16 @@ class HTMLGloboParser(HTMLParser):
 
 
 class HTMLSpuritParser(HTMLParser):
-    def __init__(
-        self,
-        *args,
-        **kwargs
-    ) -> None:
-        super().__init__(*args, **kwargs)
+    def filter(self, soup: bs4.BeautifulSoup) -> list[str]:
+        raw_variants = []
 
-    def get_script(self) -> bs4.PageElement:
-        scripts = self.soup.find_all(name="script")
-        scripts = [
+        scripts = soup.find_all(name="script")
+        ss = [
             s
             for s in scripts
             if "Spurit.CountdownTimer.snippet.productId" in s.text
         ]
-        return scripts[-1]
-
-    def filter_script(self) -> list[str]:
-        raw_variants = []
-        script = self.get_script()
+        script = ss[-1]
 
         for i in str(script.text).split(";"):
             if "variantStock[" in i:
@@ -405,9 +369,13 @@ class HTMLSpuritParser(HTMLParser):
 
         return raw_variants
 
-    def parse(self) -> list[dict]:    
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+
         inventory = []
-        for v in self.filter_script():
+        for v in self.filter(soup):
             p = v.replace("\n", "").strip().split(",")
             variant_id = p[0].split(": ")[-1]
             inventory_quantity = p[1].split(": ")[-1]
@@ -417,6 +385,233 @@ class HTMLSpuritParser(HTMLParser):
                     "variant_id": variant_id,
                     "inventory_quantity": inventory_quantity
                 }
+            )
+
+        return inventory
+
+
+class HTMLBISParser(HTMLParser):
+    def filter(self, soup: bs4.BeautifulSoup) -> list[str]:
+        script, = soup.select("script#back-in-stock-helper")
+        f = []
+
+        for line in str(script.text).split(";"):
+            if "_BISConfig.product.variants[" in line:
+                f.append(line.strip())
+        return f
+
+    def _parse_variant_ids(
+        self, soup: bs4.BeautifulSoup
+    ) -> list[int | str]:
+        variant_ids = []
+        script, = soup.select("script#em_product_variants")
+        variants = json.loads(s=script.text)
+        
+        for v in variants:
+            variant_ids.append(v["id"])
+            
+        return variant_ids
+
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+        variant_ids = self._parse_variant_ids(soup)
+        vs = self.filter(soup)
+
+        inventory = []
+
+        for v, i in zip(variant_ids, vs):
+            inventory.append(
+                {
+                    "variant_id": v,
+                    "inventory_quantity": i.split(" = ")[-1]
+                }
+            )
+        return inventory
+
+
+class HTMLSasoParser(HTMLParser):
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+        raw_variants, = soup.select("div.product div[data-product-form]")
+
+        inventory: list[dict] = []
+
+        try:
+            data = json.loads(raw_variants.attrs["data-product"])
+            
+            for v in data["variants"]:
+                inventory.append(
+                    {
+                        "variant_id": v["id"],
+                        "inventory_quantity": v["inventory_quantity"]
+                    }
+                )
+
+        except json.JSONDecodeError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
+            )
+
+        return inventory
+
+
+class HTMLGeneralParser(HTMLParser):
+    def filter(self, soup: bs4.BeautifulSoup) -> str:
+        script, = soup.select("script#product-data")
+        return script.text
+
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+        inventory: list[dict] = []
+
+        try:
+            raw_variants = json.loads(s=self.filter(soup))
+
+            for v in raw_variants["product"]["variants"]:
+                inventory.append(
+                    {
+                        "variant_id": v["id"],
+                        "inventory_quantity": v["inventory_quantity"]
+                    }
+                )
+
+        except json.JSONDecodeError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
+            )
+
+        return inventory
+
+
+class HTMLOptParser(HTMLParser):
+    def filter(self, soup: bs4.BeautifulSoup) -> str:
+        scripts = soup.select("script")
+        ss = []
+
+        for s in scripts:
+            if "var PRODUCT_JSON = fucntion() {" in s.text:
+                ss = s.text.split("\n")
+            else:
+                continue
+
+        raw_variants = ""
+        for line in ss:
+            if ("p_json = {" in line and "var p_json" not in line):
+                raw_variants = line.strip().split(" = ")[-1]
+            else:
+                continue
+
+        return raw_variants
+
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+        inventory = []
+
+        try:
+            raw_variants = json.loads(s=self.filter(soup))
+            for v in raw_variants["variants"]:
+                inventory.append(
+                    {
+                        "variant_id": v.attrs["value"],
+                        "inventory_quantity": v.attrs["data-inventory-quantity"]
+                    }
+                )
+
+        except json.JSONDecodeError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
+            )
+
+        return inventory
+
+
+class HTMLProdParser(HTMLParser):
+    def filter(self, soup: bs4.BeautifulSoup) -> str:
+        script, = soup.select("script#ProductJson-product-template")
+        return script.text
+
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(
+            markup=markup, features="html.parser"
+        )
+        inventory: list[dict] = []
+
+        try:
+            raw_variants = json.loads(s=self.filter(soup))
+
+            for v in raw_variants["variants"]:
+                inventory.append(
+                    {
+                        "variant_id": v["id"],
+                        "inventory_quantity": v["inventory_quantity"]
+                    }
+                )
+
+        except json.JSONDecodeError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
+            )
+
+        except ValueError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
+            )
+
+        return inventory
+
+
+class HTMLCamoParser(HTMLParser):
+    def filter(
+        self, soup: bs4.BeautifulSoup
+    ) -> tuple[list, list]:
+        script, = soup.select(
+            selector="script[class='camouflage-script']:not([src])"
+        )
+
+        raw_variants = ""
+        variants = []
+        inventory = []
+
+        for line in script.text.split(";"):
+            if "const camouflage_product" in line and "hide_oos_variant_qty" not in line:
+                raw_variants = line.split(" = ")[-1]
+
+            elif "camouflage_product.hide_oos_variant_qty = [" in line:
+                array_string = line.split("\n")[-1].split(" = ")[-1]
+                for i in array_string[1:-1].split(","):
+                    inventory.append(i)
+
+        vs = json.loads(raw_variants)
+        for v in vs["variants"]:
+            variants.append({"variant_id": v["id"]})
+
+        return variants, inventory
+
+    def parse(self, markup: str | bytes) -> list[dict]:
+        soup = bs4.BeautifulSoup(markup=markup, features="html.parser")
+        inventory: list[dict] = []
+
+        try:
+            variants, inventory_array = self.filter(soup)
+            for v, i in zip(variants, inventory_array):
+                v.update({"inventory_quantity": i})
+                inventory.append(v)
+
+        except json.JSONDecodeError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
+            )
+        except ValueError:
+            inventory.append(
+                {"variant_id": 0, "inventory_quantity": 0}
             )
 
         return inventory
